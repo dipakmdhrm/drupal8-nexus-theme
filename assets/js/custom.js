52585fab15b3376755e160192ee8497d19c48298
Dipak Yadav <dipakmdhrm@gmail.com>
2016-09-19 14:27:42 +0530

    Issue #2760087 by dipakmdhrm, Jaapx: Make "Select a page" translatable

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,35 +2,37 @@ jQuery(window).load(function() {
 
   /* Navigation */
 
-	jQuery('#main-menu > nav > ul').superfish({
-		delay:       500,								// 0.1 second delay on mouseout
-		animation:   { opacity:'show',height:'show'},	// fade-in and slide-down animation
-		dropShadows: true								// disable drop shadows
-	});
+  jQuery('#main-menu > nav > ul').superfish({
+    delay:       500,                // 0.1 second delay on mouseout
+    animation:   { opacity:'show',height:'show'},  // fade-in and slide-down animation
+    dropShadows: true                // disable drop shadows
+  });
 
-	jQuery('#main-menu > nav > ul').mobileMenu({
-		prependTo:'.mobilenavi'
-	});
+  jQuery('#main-menu > nav > ul').mobileMenu({
+    prependTo:'.mobilenavi',
+    groupPageText: Drupal.t('Main'),
+    topOptionText: Drupal.t('Select a page')
+  });
 
 });
 
 
 ( function() {
-	var is_webkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
-	    is_opera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
-	    is_ie     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
+  var is_webkit = navigator.userAgent.toLowerCase().indexOf( 'webkit' ) > -1,
+      is_opera  = navigator.userAgent.toLowerCase().indexOf( 'opera' )  > -1,
+      is_ie     = navigator.userAgent.toLowerCase().indexOf( 'msie' )   > -1;
 
-	if ( ( is_webkit || is_opera || is_ie ) && 'undefined' !== typeof( document.getElementById ) ) {
-		var eventMethod = ( window.addEventListener ) ? 'addEventListener' : 'attachEvent';
-		window[ eventMethod ]( 'hashchange', function() {
-			var element = document.getElementById( location.hash.substring( 1 ) );
+  if ( ( is_webkit || is_opera || is_ie ) && 'undefined' !== typeof( document.getElementById ) ) {
+    var eventMethod = ( window.addEventListener ) ? 'addEventListener' : 'attachEvent';
+    window[ eventMethod ]( 'hashchange', function() {
+      var element = document.getElementById( location.hash.substring( 1 ) );
 
-			if ( element ) {
-				if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) )
-					element.tabIndex = -1;
+      if ( element ) {
+        if ( ! /^(?:a|select|input|button|textarea)$/i.test( element.tagName ) )
+          element.tabIndex = -1;
 
-				element.focus();
-			}
-		}, false );
-	}
+        element.focus();
+      }
+    }, false );
+  }
 })();
